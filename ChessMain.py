c776f371ab9951f517fb6cb13a9a93ee5235a869
Hanxin Gao <80993827+itsnotHan@users.noreply.github.com>
2024-04-07 04:03:22 -0400

    Finished Board

--- ChessMain.py
+++ ChessMain.py
@@ -16,10 +16,10 @@ IMAGES = {}
 Initalize a global dictionary of images. This will be called exactly once in the main
 """
 
-def load_images():
+def loadImages():
     pieces = ['wP', 'wR', 'wN', 'wB', 'wK', 'wQ', 'bP', 'bR', 'bN', 'bB', 'bK', 'bQ']
     for piece in pieces:
-        IMAGES[piece] = p.transform.scale(p.image.load("images/" + piece + ".png"), (SQ_SIZE, SQ_SIZE)) 
+        IMAGES[piece] = p.transform.scale(p.image.load("images/" + piece + ".png"), (SQ_SIZE, SQ_SIZE))
         #Note: we can access an image by saying 'IMAGES['wP']'
     
 """
@@ -29,14 +29,14 @@ The main function. This will handle user input and updating graphics.
 def main():
     p.init()
     screen = p.display.set_mode((WIDTH, HEIGHT))
-    clock = p.time.clock()
+    clock = p.time.Clock()
     screen.fill(p.Color("white"))
-    gs = ChessEngine.GameState() 
-    load_images() #only do this once, before the while loop 
+    gs = ChessEngine.GameState()
+    loadImages() #only do this once, before the while loop
     running = True
     while running:
         for e in p.event.get():
-            if e.type == p.Quit:
+            if e.type == p.QUIT:
                 running = False
         drawGameState(screen, gs)
         clock.tick(MAX_FPS)
@@ -58,7 +58,11 @@ def drawBoard(screen):
             p.draw.rect(screen, color, p.Rect(c*SQ_SIZE, r*SQ_SIZE, SQ_SIZE, SQ_SIZE))
 
 def drawPieces(screen, board):
-    pass
+    for r in range(DIMENSION):
+        for c in range(DIMENSION):
+            piece = board[r][c]
+            if piece != "--": #not empty square
+                screen.blit(IMAGES[piece], p.Rect(c*SQ_SIZE, r*SQ_SIZE, SQ_SIZE, SQ_SIZE))
 
-    if __name__ == "__main__":
-        main() 
+if __name__ == "__main__":
+    main()
